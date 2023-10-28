911d9abbd074f54e27f08e35f76860e0846a452f
cheikhnouha <nouhafallbarro@gmail.com>
2023-10-28 15:54:40 +0000

    calcule  d attion,soustraction et multication

---
+++ index.js
@@ -0,0 +1,22 @@
+/* Calcule de l'addition  en  javascript*/ 
+const val =['a','b','c']
+const sum = (a,b) => {
+ return a +b;
+} 
+console.log(sum(23,45));
+
+/* Calcule de soustraction  */
+const sustraction = (d,f)=> {
+ return d - f;
+}
+console.log(sustraction(56,34));
+
+/* La multiplication d une function */
+
+const mult = (g,k) => {
+ return g * k;
+}
+console.log(mult(34,54));
+
+
+
